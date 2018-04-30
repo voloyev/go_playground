de3c7182ff02a3a65b3d0906b01e44f912ecc1d0
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-04-30 12:11:21 +0300

    Add first iteration

--- main.go
+++ main.go
@@ -1,67 +1,31 @@
 package main
 
-type Saiyan struct {
-	Name string
-	Power int
-	Father *Saiyan
-}
-// func for  struct
-// goku.Super()
-func (s *Saiyan) Super(){
-	s.Power += 1111111
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	gohan := &Saiyan {
-		Name: "Gohan",
-		Power: 10000,
-		Father: &Saiyan {
-			Name: "Goku",
-			Power: 9001,
-			Father: nil,
-		},
+	links := []string{
+		"http://google.co",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://cafephilo.lviv.ua",
+		"http://http://10.6.193.162:3000/",
+		"http://10.25.12.143:3000/",
 	}
 
-	println(gohan.Father.Power)
-
-	goku := &Saiyan {
-		Person: &Person{"Goku"},
-		Power: 9001,
-	}
-
-	goku.Introduce()
-}
-
-func oldMain() {
-	goku := new(Saiyan)
-	goku.Name = "goku"
-	goku.Power = 6060
-	goku.Father = nil
-	// same
-	// goku := &Saiyan {
-	//	Name: "goku",
-	//	power: 6060,
-	// }
-	println(goku.Power)
-}
-
-func NewSaiyan(name string, power int) Saiyan {
-	return Saiyan {
-		Name: name,
-		Power: power,
+	for _, el := range links {
+		checkLink(el)
 	}
 }
 
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "it might be down")
+		return
+	}
 
-type Person struct {
-	Name string
-}
-
-func (p *Person) Introduce() {
-	fmt.Printf("Hi, I'm %s\n", p.Name)
-}
-
-type Saiyan struct {
-	*Person
-	Power int
+	fmt.Println(link, "is up")
 }
--- persone.go
+++
@@ -1,3 +0,0 @@
-package person
-import "fmt"
-
