381d36534c96249b04b7e5e2773f74260200353e
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-14 17:02:16 +0300

    Play with go closure

--- main.go
+++ main.go
@@ -1,67 +1,14 @@
 package main
 
-type Saiyan struct {
-	Name string
-	Power int
-	Father *Saiyan
-}
-// func for  struct
-// goku.Super()
-func (s *Saiyan) Super(){
-	s.Power += 1111111
-}
-
 func main() {
-	gohan := &Saiyan {
-		Name: "Gohan",
-		Power: 10000,
-		Father: &Saiyan {
-			Name: "Goku",
-			Power: 9001,
-			Father: nil,
-		},
+	addN := func(m int) func(int) int {
+		return func(n int) int {
+			return m + n
+		}
 	}
 
-	println(gohan.Father.Power)
-
-	goku := &Saiyan {
-		Person: &Person{"Goku"},
-		Power: 9001,
-	}
-
-	goku.Introduce()
-}
-
-func oldMain() {
-	goku := new(Saiyan)
-	goku.Name = "goku"
-	goku.Power = 6060
-	goku.Father = nil
-	// same
-	// goku := &Saiyan {
-	//	Name: "goku",
-	//	power: 6060,
-	// }
-	println(goku.Power)
-}
-
-func NewSaiyan(name string, power int) Saiyan {
-	return Saiyan {
-		Name: name,
-		Power: power,
-	}
-}
-
-
-type Person struct {
-	Name string
-}
-
-func (p *Person) Introduce() {
-	fmt.Printf("Hi, I'm %s\n", p.Name)
-}
-
-type Saiyan struct {
-	*Person
-	Power int
+	addFive := addN(5)
+	result := addN(6)
+	println(addFive)
+	println(result)
 }
