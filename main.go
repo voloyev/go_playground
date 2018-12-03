a0348c2fefa6a877b5403b5d86c8e9339f1b0a81
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-12-03 18:26:01 +0200

    Add interface

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
-func main() {
-	addN := func(m int) func(int) int {
-		return func(n int) int {
-			return m + n
-		}
-	}
+import (
+	"fmt"
+)
 
-	addFive := addN(5)
-	result := addN(6)
-	println(addFive)
-	println(result)
+type MyFile interface {
+	Read() (string, error)
+	Write(content string) (string, error)
+}
+
+// TODO rewrite interface for reference too
+
+type defFile struct {
+	name    string
+	content string
+}
+
+func (d defFile) Read() (string, error) {
+	return d.content, nil
+}
+
+func (d defFile) Write(content string) (string, error) {
+	d.name = content
+	return "success", nil
+}
+
+func getName(ff *defFile) string {
+	ff_name, _ := ff.Read()
+	return ff_name
+}
+func main() {
+	ff := defFile{"bob", "content"}
+	fmt.Println(getName(&ff))
+
+	ff.Write("nooooooo")
+	fmt.Println(getName(&ff))
 }
