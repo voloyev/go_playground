ba9ba1b73d6e3de255c4917d0f19a6982bde7cae
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-12-05 23:23:25 +0200

    Add programs from pakt book

---
+++ simple_programs/simpleweb/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte(`<html>
+                            <head>
+                        <title> Chat </title>
+                    </head>
+                <body>
+            Lets chat
+        </body>
+    <html>`))
+    })
+
+    if  err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
