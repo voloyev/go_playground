ee7af49e6e942a5206a85c4ef2baf9dd01cd1311
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-12-06 14:05:19 +0200

    Add templates

--- simple_programs/simpleweb/main.go
+++ simple_programs/simpleweb/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte(`<html>
-                            <head>
-                        <title> Chat </title>
-                    </head>
-                <body>
-            Lets chat
-        </body>
-    <html>`))
-    })
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
 
-    if  err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
